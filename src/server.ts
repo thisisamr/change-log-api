0d5b4d80db4e8acb25b2a999371ca381f679bb5d
asolyma <asolyma@hotmail.com>
2022-10-05 22:44:40 +0200

    added more validation

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,22 @@ import morgan from "morgan";
 import { protect } from "./modules/auth";
 import { createNewUser, signin } from "./services/user";
 import { json } from "express";
+import { body, checkSchema } from "express-validator";
+import { validateUserInput } from "./middleware";
 const app = express();
 app.use(json());
 app.use(urlencoded());
 app.use(morgan("dev"));
 app.use("/api/v1", protect, router);
-app.post("/user", createNewUser);
+app.post(
+  "/user",
+  checkSchema({
+    username: { isString: true },
+    password: { isStrongPassword: true },
+    email: { isEmail: true },
+  }),
+  validateUserInput,
+  createNewUser
+);
 app.post("/login", signin);
 export default app;
